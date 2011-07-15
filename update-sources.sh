0e27c296070ae9baef3f9e90288edfc9d436d8f6
Mika Bostrom <mika.bostrom@nomovok.com>
2011-07-15 16:10:26 +0300

    Helper script: revision fixes
    This is the first step in fixing the module revision mess. Lot of the
    changes come from Carsten Munk.
    
    - Major thinko fix: use HEAD instead of module master. We had problems
      with sources never seeming to be in perfect sync. The problem was that
      the updater script always created the archive from git master.
      However, HEAD didn't point to that every time. This triggered a lot of
      confusion and plenty of weird build failures.
    - Use V8 submodule in qtdeclarative tree. When exporting the module,
      submodules are not pulled in. Thus the earlier need for external v8
      source tarball. Now the sources are pulled directly from embedded
      tree's HEAD, making sure the version is the same declarative has been
      developed and synced against. Since this local tree also includes the
      Qt patches, we can drop application of patches against V8 tree. Should
      make the spec even clearer.
    - Prepare for major spec update: the version in spec files should not be
      hard-coded. With these changes we can rewrite specs to use a
      separately defined value for version string and simply modify the
      specs in OBS tree to contain the correct versions. Such a change
      should make spec changes smaller and thus easier in the future.
    - Prepare script to use osc's automatic file update mechanism.
    - Make file copying visible by use of "-v" flag. Works as a combined
      progress indicator and debug output.

--- update-sources.sh
+++ update-sources.sh
@@ -48,6 +48,18 @@ function put_last() {
     echo ${_rev} > ${HOME}/.modular-qt5-revs/${_mod}
 }
 
+# GIT_DIR has been set when this function is called
+function get_version() {
+    if ! git describe >/dev/null 2>&1; then
+        count=$(git rev-list HEAD | wc -l)
+        realcommitid=$(git describe --always)
+        commitid=$count.g$realcommitid
+    else
+        commitid=$(expr match $(git describe) '.*-\([0-9]*-g[a-z0-9]*\)$' \
+                | tr - .)
+    fi
+    echo $commitid
+}
 
 # Update OBS project
 (cd ${OBSDIR}; osc update)
@@ -63,20 +75,41 @@ for m in ${QT5_MODULES}; do
     bn="qt5-${m:2}"
     export GIT_DIR=${QT5_DIR}/${m}/.git
     last=$(get_last ${m})
-    head=$(git show | head -n 1 | sed s'/^commit //')
+    head=$(git show HEAD | head -n 1 | sed s'/^commit //')
     if [ "${head}" != "${last}" ]; then
-        git archive master --prefix=${bn}/ | gzip > ${OBSDIR}/${m}/${bn}.tar.gz
+        ver=$(get_version)
+        git archive HEAD --prefix=${bn}/ | gzip > ${OBSDIR}/${m}/${bn}-5~${ver}.tar.gz
         # Store the revision used
         put_last ${m} ${head}
+        # QtDeclarative needs v8 sources
+        if [ ${m} = "qtdeclarative"]; then
+            # Redefine for get_version
+            export GIT_DIR=${QT5_DIR}/${m}/src/3rdparty/v8/.git
+            v8id=$(git describe --always)
+            v8old=$(get_last v8)
+            if [ "${v8id}" != ${v8old} ]; then
+                if [ ! -e ${OBSDIR}/${m}/v8-git${v8id}.tar.gz ]; then
+                    git archive HEAD --prefix=v8/ | gzip > ${OBSDIR}/${m}/v8-git${v8id}.tar.gz
+                fi
+                put_last v8 ${v8id}
+            fi
+        fi
     fi
     
     # Spec, rpmlintrc, patches, extra files,
     # all from this directory
-    cp ${m}/*.spec ${OBSDIR}/${m}/
+    cp -v ${m}/*.spec ${OBSDIR}/${m}/
     if [ -d ${m}/files ]; then
-        cp ${m}/files/* ${OBSDIR}/${m}/
+        cp -v ${m}/files/* ${OBSDIR}/${m}/
         osc add ${OBSDIR}/${m}/* 2>/dev/null
     fi
+
+    sed -i "s/define _qtmodule_snapshot_version %nil/define _qtmodule_snapshot_version 5~git${ver}/g" ${OBSDIR}/${m}/*.spec
+    if [ ${m} = "qtdeclarative" ]; then
+        sed -i "s/define _v8_snapshot_version %nil/define _v8_snapshot_version ${v8id}/g" ${OBSDIR}/${m}/*.spec
+    fi
+
+    (cd ${OBSDIR}/${m}/ && osc addremove)
 done
 
 # And finally commit all the modules at once
